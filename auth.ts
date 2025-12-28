aec37e1047e2aa5853878d27cfa4f54251ee9ffd
Momoka Kamijo <moowfaw@gmail.com>
2025-12-28 23:56:26 +0900

    feat: Slack APIからdisplay_nameを取得してカラムに保存

--- auth.ts
+++ auth.ts
@@ -12,10 +12,21 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   secret: process.env.AUTH_SECRET,
   callbacks: {
     async signIn({ profile }) {
+      const res = await fetch(
+        // ログインしたユーザーのプロフィール情報を取得
+        `https://slack.com/api/users.profile.get?user=${profile?.sub}`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`,
+          },
+        }
+      );
+      const data = await res.json();
+
       await upsertUserFromSlack({
         slack_user_id: profile?.sub as string,
         slack_email: profile?.email as string,
-        slack_display_name: profile?.name as string,
+        slack_display_name: data.profile.display_name,
         slack_image: profile?.picture,
       });
       return true;
