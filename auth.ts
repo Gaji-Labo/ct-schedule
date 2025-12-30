7cfafe49ba47a546b0b527eeee8e3cf6b081061b
Momoka Kamijo <moowfaw@gmail.com>
2025-12-30 22:45:02 +0900

    debug: signInのcallbackを無効化

--- auth.ts
+++ auth.ts
@@ -1,4 +1,3 @@
-import { upsertUserFromSlack } from "@/app/actions";
 import NextAuth from "next-auth";
 import Slack from "next-auth/providers/slack";
 
@@ -11,24 +10,26 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   ],
   secret: process.env.AUTH_SECRET,
   callbacks: {
-    async signIn({ profile }) {
-      const res = await fetch(
-        // ログインしたユーザーのプロフィール情報を取得
-        `https://slack.com/api/users.profile.get?user=${profile?.sub}`,
-        {
-          headers: {
-            Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`,
-          },
-        }
-      );
-      const data = await res.json();
+    async signIn() {
+      // const res = await fetch(
+      //   // ログインしたユーザーのプロフィール情報を取得
+      //   `https://slack.com/api/users.profile.get?user=${profile?.sub}`,
+      //   {
+      //     headers: {
+      //       Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`,
+      //     },
+      //   }
+      // );
+      // const data = await res.json();
 
-      await upsertUserFromSlack({
-        slack_user_id: profile?.sub as string,
-        slack_email: profile?.email as string,
-        slack_display_name: data.profile.display_name,
-        slack_image: profile?.picture,
-      });
+      // await upsertUserFromSlack({
+      //   slack_user_id: profile?.sub as string,
+      //   slack_email: profile?.email as string,
+      //   slack_display_name: data.profile.display_name,
+      //   slack_image: profile?.picture,
+      // });
+
+      console.log("signIn callback executed");
       return true;
     },
     async jwt({ token, account, profile }) {
