d062d858a284ac478ad3996b3dd3137b36836430
Momoka Kamijo <moowfaw@gmail.com>
2025-12-30 09:49:14 +0900

    feat: ダイアログ内のUI作成

--- src/app/components/EmployeeNumberDialog.tsx
+++ src/app/components/EmployeeNumberDialog.tsx
@@ -1,11 +1,21 @@
 "use client";
 
 import { setEmployeeNumber } from "@/app/actions";
-import { Dialog } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import { toast } from "sonner";
 
 export const EmployeeNumberDialog = ({ userId }: { userId: string }) => {
+  const [number, setNumber] = useState("");
   const [open, setOpen] = useState(true);
 
   const handleSubmit = async (formData: FormData) => {
@@ -20,5 +30,39 @@ export const EmployeeNumberDialog = ({ userId }: { userId: string }) => {
     }
   };
 
-  return <Dialog></Dialog>;
+  return (
+    <Dialog
+      open={open}
+      onOpenChange={(open) => {
+        if (!open) setNumber("");
+        setOpen(open);
+      }}
+    >
+      <DialogContent className="sm:max-w-[425px]">
+        <form action={handleSubmit} className="grid gap-5">
+          <DialogHeader>
+            <DialogTitle>社員番号を入力</DialogTitle>
+            <DialogDescription>社員番号を入力してください</DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Input
+                name="employeeNumber"
+                type="number"
+                id="employeeNumber"
+                value={number}
+                onChange={(e) => setNumber(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit" disabled={!number}>
+              決定
+            </Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
 };
