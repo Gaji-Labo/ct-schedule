786f2aabbcb0b16e440b4f7ab1cbb479dbf61e73
Momoka Kamijo <moowfaw@gmail.com>
2026-01-27 22:50:35 +0900

    feat: Slackに投稿するメッセージを生成

---
+++ src/app/api/cron/notify-ct-pairs/route.ts
@@ -0,0 +1,11 @@
+import type { CT } from "@/src/utils/member";
+
+function buildMessage(ct: CT): string {
+  const lines = ["📅 本日のCTペア", ""];
+  const pairLines = ct.round.map(([user1, user2]) => {
+    const pair1 = user1.slack_display_name;
+    const pair2 = user2 ? user2.slack_display_name : "お休み";
+    return `- ${pair1} ${pair2}`;
+  });
+  return [...lines, ...pairLines].join("\n");
+}
