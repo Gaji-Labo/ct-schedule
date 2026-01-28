73af9000ce7fafe82fc5a2cd98d59c25c78f9214
Momoka Kamijo <moowfaw@gmail.com>
2026-01-28 11:40:44 +0900

    fix: メンションの位置修正

--- src/app/api/cron/notify-ct-pairs/route.ts
+++ src/app/api/cron/notify-ct-pairs/route.ts
@@ -42,11 +42,11 @@ export async function GET(request: Request) {
 }
 
 function buildMessage(ct: CT): string {
-  const lines = ["📅 本日のCTペア", ""];
+  const lines = ["🗣️ 本日のCTペア", ""];
   const pairLines = ct.round.map(([user1, user2]) => {
-    const pair1 = user1.slack_display_name;
-    const pair2 = user2 ? user2.slack_display_name : "お休み";
-    return `- @${pair1} @${pair2}`;
+    const pair1 = `@${user1.slack_display_name}`;
+    const pair2 = user2 ? `@${user2.slack_display_name}` : "お休み";
+    return `• ${pair1} ${pair2}`;
   });
   return [...lines, ...pairLines].join("\n");
 }
